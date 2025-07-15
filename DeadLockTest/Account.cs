e2dad6ba2aa817d281e436e9a6efe364a1ed7ae2
ruslan.slobodin <ruslan.slobodin@ic-group.ru>
2025-07-15 23:04:50 +0300

    Добавьте файлы проекта.

---
+++ DeadLockTest/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadLockTest
+{
+    public class Account
+    {
+        public int AccountNumber; 
+        public decimal Amount; 
+        public string ownerName;
+    }
+
+    public class TransferHelper
+    {
+        public void TransferLock(Account a, Account b, decimal amount)
+        {
+            lock (a)
+            {
+                Thread.Sleep(5000);
+                lock (b)
+                {
+                    
+                    a.Amount -= amount;
+                    b.Amount += amount;
+                }
+            }
+        }
+
+        public void TransferSuccess(Account a, Account b, decimal amount)
+        {
+            var first = a.AccountNumber < b.AccountNumber ? a : b;
+            var second = a.AccountNumber < b.AccountNumber ? b : a;
+
+            lock (first)
+            {
+                Thread.Sleep(5000);
+                lock (second)
+                {
+
+                    a.Amount -= amount;
+                    b.Amount += amount;
+                }
+            }
+        }
+    }
+}
---
+++ Tests/TransferHelperTest.cs
@@ -0,0 +1,43 @@
+﻿using DeadLockTest;
+
+namespace Tests
+{
+    public class TransferHelperTest
+    {
+        [Fact]
+        public async Task DeadlockTest()
+        {
+            var ivan = new Account() { AccountNumber = 1, Amount = 1000, ownerName = "Ivan" };
+            var olga = new Account() { AccountNumber = 2, Amount = 500, ownerName = "Olga" };
+
+            TransferHelper helper = new TransferHelper();
+
+            var task1 = Task.Run(() => helper.TransferLock(ivan, olga, 100));
+            var task2 = Task.Run(() => helper.TransferLock(olga, ivan, 10));
+
+            await Task.WhenAll(task1, task2);
+
+            Assert.Equal(910, ivan.Amount);
+            Assert.Equal(590, olga.Amount);
+
+        }
+
+        [Fact]
+        public async Task NormalTest()
+        {
+            var ivan = new Account() { AccountNumber = 1, Amount = 1000, ownerName = "Ivan" };
+            var olga = new Account() { AccountNumber = 2, Amount = 500, ownerName = "Olga" };
+
+            TransferHelper helper = new TransferHelper();
+
+            var task1 = Task.Run(() => helper.TransferSuccess(ivan, olga, 100));
+            var task2 = Task.Run(() => helper.TransferSuccess(olga, ivan, 10));
+
+            await Task.WhenAll(task1, task2);
+            Assert.Equal(910, ivan.Amount);
+            Assert.Equal(590, olga.Amount);
+
+        }
+
+    }
+}
